fd3a486c37bdde32fc8600ee9b083168d0655b6a
deigo <rojaspachecodiegofernando@gmail.com>
2023-10-26 16:50:24 -0500

    Front

---
+++ blockehr/src/main/java/com/blockehr/blockehr/BlockehrApplication.java
@@ -0,0 +1,13 @@
+package com.blockehr.blockehr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockehrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockehrApplication.class, args);
+	}
+
+}
---
+++ blockehr/src/test/java/com/blockehr/blockehr/BlockehrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blockehr.blockehr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockehrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
